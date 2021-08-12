6110f5a087ce8fa7c5110138e293926119057584
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2021-08-12 12:58:33 +0200

    Start coding foodOrder app

---
+++ src/screens/LandingScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+
+const LandingScreen = () => {
+    return(
+        <View>
+            <View>
+                <Text>Navigation</Text>
+            </View>
+            <View>
+                <Text>
+                    Body
+                </Text>
+            </View>
+            <View>
+                <Text>Footer</Text>
+            </View>
+        </View>
+    )
+}
+
+export default LandingScreen;
