5891c9562b7d341c7d6d4679787d427b4530f146
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2021-08-16 15:12:11 +0200

    Coding LandingScreen

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,22 +1,67 @@
-import React from 'react'
-import { View, Text} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import * as Location from 'expo-location';
+
+const screenWidth = Dimensions.get("screen").width;
 
 const LandingScreen = () => {
-    return(
-        <View>
-            <View>
-                <Text>Navigation</Text>
-            </View>
-            <View>
-                <Text>
-                    Body
-                </Text>
-            </View>
-            <View>
-                <Text>Footer</Text>
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.navigation} />
+      <View style={styles.body}>
+        <Image
+          source={require("./../images/delivery_icon.png")}
+          style={styles.deliveryIcon}
+        />
+        <View style={styles.addressContainer}>
+          <Text style={styles.addressTitle}>Your delivery address</Text>
         </View>
-    )
-}
+          <Text style={styles.addressText}>Waiting for current location</Text>
+      </View>
+      <View style={styles.footer} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  navigation: {
+    flex: 2,
+  },
+  body: {
+    flex: 9,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  deliveryIcon: {
+    width: 120,
+    height: 120,
+    marginBottom: 10
+  },
+  addressContainer: {
+    width: screenWidth - 100,
+    borderBottomColor: 'red',
+    borderBottomWidth: 0.5,
+    padding: 5,
+    marginBottom: 10,
+    alignItems: "center"
+  },
+  addressTitle: {
+      fontWeight: "200",
+      color: "#7D7D7D",
+      fontSize: 22
+  },
+  addressText: {
+      fontSize: 20,
+      fontWeight: "200",
+      color: "#4f4f4f"
+  },
+  footer: {
+    flex: 1,
+  },
+});
 
 export default LandingScreen;
